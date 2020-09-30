d912d6aecbc2ab34427cde64c10ca6b47e565610
Bertrand Shao <shaob@g.ucla.edu>
2020-09-30 10:02:21 -0700

    Did anagrams

--- allAnagrams/allAnagrams.js
+++ allAnagrams/allAnagrams.js
@@ -12,7 +12,44 @@
   * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-var allAnagrams = function(string) {
+  var allAnagrams = function(string) {
+    //declare an results object
+    //declare an initial anagram string
+    const results = {};
+    
+    //innner recursive 
+    const inner = function(anagram, rem) {
+      //base case -> anagram.length === string length
+        //add anagram to results object -> object.anagram = true; 
+      if (anagram.length === string.length) {
+        results[anagram] = true;
+      }
+      
+      //recursive case
+      //use slice to grab remainding options from rem
+      for (let i = 0; i < rem.length; i++) {
+        var options = rem.slice(0, i) + rem.slice(i+1);      
+        inner(anagram+rem[i], options)
+      }
+
+    }
+    //invoke inner recursive function
+    inner('', string);
+  
+    //return the non repeated anagrams (keys of the object)
+    return Object.keys(results);
+  }
 
 
-};
+//input: string, with letters, no spaces
+//output: array of strings, same length, no repetition
+//constraints: letters do not repeat
+//edge cases: if empty string, output empty array
+
+// [ 'abcd', 'adcb', 'acdb', 'abdc', 'bac', 'bca', 'cab', 'cba' ]
+
+//create a result array
+//loop over the string
+ //create word variable
+ //set first letter of word variable to be string[i]
+  //
