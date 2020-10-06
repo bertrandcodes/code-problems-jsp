38da82d457a01d9e29b7bf1e0f3e91244dc95b4e
C Taylor <30055120+christineetaylor@users.noreply.github.com>
2020-10-06 08:28:55 -0700

    W2D2

---
+++ threeSums/threeSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+/* 15. 3Sum: https://leetcode.com/problems/missing-number/
+
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Notice that the solution set must not contain duplicate triplets.
+
+NOTE: This is an essential interview prompt, one which allows you to demonstrate your ability to reduce time complexity. Solve this in whatever way you are able to today, using "brute force" (ignoring efficiency) if necessary. We will revisit this problem later in JSP, after re/learning algorithms that will help you approach this problem with time complexity in mind.
+
+*/
+
+
+var threeSum = function(nums) {
+    
+};
