73cf2d5ee0818becf5902fcc1be0f9ccb9d1b7c5
Bertrand Shao <shaob@g.ucla.edu>
2020-10-05 17:08:29 -0700

    Finish missing number

--- missingNumber/missingNumber.js
+++ missingNumber/missingNumber.js
@@ -13,5 +13,17 @@ Follow up: Could you implement a solution using only O(1) extra space complexity
 
 
 var missingNumber = function(nums) {
-    
+    nums.sort((a,b) => {return a-b})
+    for (let i = 0; i < nums.length; i++){
+        if ( i !== nums[i] ) return i;
+    }
+    return nums[nums.length - 1] + 1
 };
+
+// input: array from 0 to n, unique numbers
+// output: return the number that is missing from the array
+// constraints: O(n)
+// edge cases: no missing numbers?
+
+// iterate over nums
+ // if i does not equal nums[i] then return i
