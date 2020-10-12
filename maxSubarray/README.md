5d935abd6da70bcd5884c16dba0de17f27bb7293
C Taylor <30055120+christineetaylor@users.noreply.github.com>
2020-10-12 07:56:23 -0700

    W3D1

---
+++ maxSubarray/README.md
@@ -0,0 +1,17 @@
+# Welcome to Week 3 of JSP!
+
+It is Dynamic Programming Week for toy problems. This type of interview prompt is very common, because it allows the interviewer to view your thought process in wrangling potentially high time/space complexity problems into an optimized set of sub-problems.
+
+If you need a refresher on dynamic programming, start with these resources before beginning.
+
+* [Demystifying Dynamic Programming](https://www.freecodecamp.org/news/demystifying-dynamic-programming-3efafb8d4296/) | Alaina Kafkes
+
+* [Dynamic Programming playlist](https://www.youtube.com/playlist?list=PLVrpF4r7WIhTT1hJqZmjP10nxsmrbRvlf)  by Andrey Grehov
+
+
+
+
+
+
+
+
---
+++ maxSubarray/maxSubarray.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+/* 53. Maximum Subarray
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+
+Example 2:
+
+Input: intervals = [[1,4],[4,5]]`
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+*/
+
+var maxSubArray = function(nums) {
+    
+};
