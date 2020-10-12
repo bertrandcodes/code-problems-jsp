5ee74be54a5035845cfac17183e0ed98f3214950
C Taylor <30055120+christineetaylor@users.noreply.github.com>
2020-10-12 08:11:46 -0700

    W3D1

--- maxSubarray/maxSubarray.js
+++ maxSubarray/maxSubarray.js
@@ -4,7 +4,7 @@
  */
 
 /* 53. Maximum Subarray
-Solve in IDE or on Leetcode: https://leetcode.com/problems/missing-number/
+Solve in IDE or on Leetcode: https://leetcode.com/problems/maximum-subarray/
 
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
