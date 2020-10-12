aef98f7306f42df1346b8a91d330996d5b96589f
C Taylor <30055120+christineetaylor@users.noreply.github.com>
2020-10-12 09:01:43 -0700

    W3D1

--- maxSubarray/maxSubarray.js
+++ maxSubarray/maxSubarray.js
@@ -12,27 +12,29 @@ Follow up: If you have figured out the O(n) solution, try coding another solutio
 
  
 Example 1:
-
 Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
-Example 2:
 
+
+Example 2:
 Input: nums = [1]
 Output: 1
-Example 3:
 
+
+Example 3:
 Input: nums = [0]
 Output: 0
-Example 4:
 
+
+Example 4:
 Input: nums = [-1]
 Output: -1
-Example 5:
 
+
+Example 5:
 Input: nums = [-2147483647]
 Output: -2147483647
- 
 
 Constraints:
 
